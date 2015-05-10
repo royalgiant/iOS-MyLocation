57cf70243b920024f5fd8478ee65324f5144536a
Donald Lee <donaldlee50@gmail.com>
2015-05-09 22:10:03 -0700

    Data store

---
+++ Location.swift
@@ -0,0 +1,22 @@
+//
+//  Location.swift
+//  
+//
+//  Created by Donald Lee on 2015-05-09.
+//
+//
+
+import Foundation
+import CoreData
+import CoreLocation
+
+class Location: NSManagedObject {
+
+    @NSManaged var latitude: Double
+    @NSManaged var longitude: Double
+    @NSManaged var date: NSDate
+    @NSManaged var locationDescription: String
+    @NSManaged var category: String
+    @NSManaged var placemark: CLPlacemark?
+
+}
